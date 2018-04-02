24a30cc42d11732dc6dfd7d6e87070bdff46002e
Skayo <jonasg.cool@gmail.com>
2018-04-02 21:01:22 +0200

    Fixed some things

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./semicolon.js');
--- semicolon.js
+++ semicolon.js
@@ -1,17 +1,23 @@
 (function () {
 
-  var semicolon = function() { return ';'; };
+	var semicolon = function() { return ';'; };
 
-  semicolon.law = function() { return 'Makes you chilled all the time.'; };
+	semicolon.law = function() { return 'Makes you chilled all the time.'; };
 
-  semicolon.english = function() { return 'semicolon'; };
-  semicolon.korean = function() { return '세미콜론'; };
-  semicolon.twins = function() { return ';'; }; //greek question mark
-  semicolon.length = function() { return '1'; };
-                          
-  //todo: not sure how it works
-  semicolon.isSemicolon = function(a) { return a === semicolon(); };
+	semicolon.english = function() { return 'semicolon'; };
+	semicolon.korean = function() { return '세미콜론'; };
+	semicolon.twins = function() { return ';'; }; //greek question mark
+	semicolon.len = function() { return 1; };
 
- 
+	//todo: not sure how it works (Seriously?)
+	semicolon.isSemicolon = function(a) { return a === semicolon(); };
 
+
+	if(typeof module !== 'undefined' && module.exports) {
+		module.exports = semicolon;
+	} else if (typeof define === 'function' && define.amd){
+		define(semicolon);
+	} else if (window) {
+		window.semicolon = semicolon;
+	}
 }());
